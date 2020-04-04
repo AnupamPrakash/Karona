b4a96d4e277f793691543a3d9361a1ba0473a6eb
subhash0x <subhash0x@gmail.com>
2020-04-04 11:53:27 +0530

    buggy life

--- app/src/main/java/com/darkworld/karona/MainActivity.java
+++ app/src/main/java/com/darkworld/karona/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
